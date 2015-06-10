024ff98da34b8d9c3b08557952738de89c92a8a4
Ivan-Udakara <ivanud.12@cse.mrt.ac.lk>
2015-06-10 21:14:49 +0530

    login form completed. need to add the common template

--- moodle/index.php
+++ moodle/index.php
@@ -1,3 +1,24 @@
+<?php
+    define('DB_NAME', 'moodle');
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+
+    function getName(){
+        // The string to establish the connection
+        $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die("Failed to connect to the database. Error code:1");
+        // Select the database
+        $db = mysqli_select_db($con, DB_NAME) or die("Failed to connect to the database. Error code:2");
+
+        // The query to get the data from the database
+        $query = mysqli_query($con, "SELECT student.name FROM student WHERE student.st_id = '".$_SESSION['st_id']."'");
+        // Receive a raw from the database that satisfy the above condition and stores in "$row"
+        $row = mysqli_fetch_array($query, MYSQLI_ASSOC) or die("Can not receive the data from the database");
+
+        return $row['name'];
+    }
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,7 +32,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        session_start();
+        echo getName();
+        echo '('.$_SESSION['st_id'].')';
         ?>
     </body>
 </html>
--- moodle/login.php
+++ moodle/login.php
@@ -6,43 +6,76 @@
 -->
 
 <?php
-    //  Information to connect to the database
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'moodle';
-
-    //  Creates a connection to the database
-    $conn = new mysqli($server, $username, $password, $database);
-
-    function getPassword($id, $conn){
-        //sql statements to get the password details
-        $sql_studentPW = "select student.password from student where student.st_id = '".$id."'";
-        //$sql_lecPW = "select lecturer.password from lecturer where lecturer.lec_id = '".$id."'";
-
-        $message = '';
-
-        //check the connection
-        if($conn -> connect_error){
-            $message = $conn -> connect_error;
-            die('Connection error:'.$message);
-        }
-        else{
-            ////if($conn -> query($sql_studentPW)){
-                // Extracts all the relevant basic information into $result
-                $result = $conn -> query($sql_studentPW);
-            ////}
-            /*else{
-                // Extracts all the relevant basic information into $result
-                $result = $conn -> query($sql_lecPW);
-            }*/
+    define('DB_NAME', 'moodle');
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
 
 
-            // If an error occurs while extracting information
-            if($conn -> error){
-                die('Error occurred:'.$conn -> error);
+    function getPassword($id, $pwd){
+
+        // Start a session for the user login
+        session_start();
+
+        if(!empty($id) AND !empty($pwd)){
+
+            // The string to establish the connection
+            $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die("Failed to connect to the database. Error code:1");
+            // Select the database
+            $db = mysqli_select_db($con, DB_NAME) or die("Failed to connect to the database. Error code:2");
+
+            // The query to get the data from the database
+            $query_st = mysqli_query($con, "SELECT student.password,student.st_id FROM student WHERE student.st_id = '".$id."'");
+            // Receive a raw from the database that satisfy the above condition and stores in "$ro_stw"
+            //////$row_st = mysqli_fetch_array($query_st, MYSQLI_ASSOC) or die("Can not receive the data from the database");
+
+            // The query to get the data from the database
+            $query_lec = mysqli_query($con, "SELECT lecturer.password,lecturer.lec_id FROM lecturer WHERE lecturer.lec_id = '".$id."'");
+            // Receive a raw from the database that satisfy the above condition and stores in "$ro_stw"
+            //////$row_lec = mysqli_fetch_array($query_lec, MYSQLI_ASSOC) or die("Can not receive the data from the database");
+
+            /*
+             * Checks whether the 'password' index of the returned array is non-empty and
+             * whether it is similar to the password entered by the user
+            */
+            if(mysqli_fetch_array($query_st, MYSQLI_ASSOC)){
+                echo '***********************************************';
+                $query_st = mysqli_query($con, "SELECT student.password,student.st_id FROM student WHERE student.st_id = '".$id."'");
+                $row_st = mysqli_fetch_array($query_st, MYSQLI_ASSOC) or die("Can not receive the data from the database");
+                if($row_st['password'] == $pwd){
+                    $_SESSION['st_id'] = $row_st['st_id'];
+                    echo "Access granted.";
+                    /*// Calls the function redirect
+                    redirect();*/
+                    header("Location: index.php");
+                    exit();
+                }
             }
+            elseif(mysqli_fetch_array($query_lec, MYSQLI_ASSOC)){
+                $query_lec = mysqli_query($con, "SELECT lecturer.password,lecturer.lec_id FROM lecturer WHERE lecturer.lec_id = '".$id."'");
+                $row_lec = mysqli_fetch_array($query_lec, MYSQLI_ASSOC) or die("Can not receive the data from the database");;
+                if($row_lec['password'] == $pwd){
+                    $_SESSION['lec_id'] = $row_st['lec_id'];
+                    echo "Access granted.";
+                    /*// Calls the function redirect
+                    redirect();*/
+                    header("Location: view_lecturer.php");
+                    exit();
+                }
+            }
+            else{
+                echo "<script type='text/javascript'>alert('Password and Id are incorrect. Try again.');</script>";
+            }
+
+            // Closes the connection crested above
+            mysqli_close($con);
         }
+    }
+
+    // Function to redirect to index.php
+    function redirect(){
+        header("Location: index.php");
+        exit();
     }
 ?>
 
@@ -64,32 +97,30 @@
                 <div class="col-sm-3">
                     <form role="form" method="post">
                         <div class="form-group">
-                            <input type="id" class="form-control" id="id" placeholder="ID" name="id">
+                            <input type="id" maxlength="7" class="form-control" id="id" placeholder="ID" name="id">
                         </div>
                         <div class="form-group">
-                            <input type="text" size="25" class="form-control" id="pwd" placeholder="Password" name="pwd">
+                            <input type="password" size="25" maxlength="25" class="form-control" id="pwd" placeholder="Password" name="pwd">
                         </div>
                         <button type="submit" class="btn btn-default" name="login">
                             Login<!--add a image to the button-->
                         </button>
                     </form>
                 </div>
-                <div class="col-sm-1">
-                    <!--<button type="submit" class="btn btn-default" name="login">
-                       Login<!--add a image to the button--
-                    </button>-->
-                </div>
+
 
             </div>
 
             <div class="col-sm-4">
             <?php
-            print_r($_POST);
             if(isset($_POST['login'])){
+                //Stores the user entered values in to 2 variables
                 $id = $_POST['id'];
                 $pwd = $_POST['pwd'];
 
-                getPassword($id, $conn);
+                // Calls the getPassword function
+                getPassword($id, $pwd);
+
             }
 
             ?>
